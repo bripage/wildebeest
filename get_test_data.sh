9c46a85bc7f8924efa3df4ede846c3cb8c1ac1ba
Brian Page <bripage@users.noreply.github.com>
2021-07-18 15:47:09 -0400

    .

--- get_test_data.sh
+++ get_test_data.sh
@@ -2,9 +2,10 @@
 
 mkdir test_data
 cd test_data
-wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a8a -o a8a_train.dat
-wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a8a.t a8a_test.dat
-
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a8a
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a8a.t
+mv a8a a8a_train.dat
+mv a8a.t a8a_test.dat
 ../utilities/shuffle -i a8a_train.dat > a8a_train.shuf
 ../utilities/libsvm2csv -i a8a_train.shuf -s 22696 > a8a_train.csv
 ../utilities/libsvm2csv -i a8a_test.dat -s 9865 > a8a_test.csv
