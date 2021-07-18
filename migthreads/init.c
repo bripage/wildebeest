54f70ae9ae1789c37ceee3295aa2fafc0db79657
Brian Page <bripage@users.noreply.github.com>
2021-07-18 16:34:59 -0400

    Update init.c

--- migthreads/init.c
+++ migthreads/init.c
@@ -25,81 +25,57 @@ void parse_args(int argc, char * argv[]) {
 
     for (i = 1; i < argc; i++){
         if (!strcmp(argv[i],"--trainers")){
-            num_arg = atoi(argv[i+1]) + 1;
+            num_arg = atoi(argv[i+1]);
             mw_replicated_init(&threads, num_arg);
-            printf("Trainer Count = %d\n",threads);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--train-data")){
             train_data_path = (char *) malloc(strlen(argv[i+1]) * sizeof(char));
             strcpy(train_data_path, argv[i+1]);
-            printf("train_data_path = %s\n",train_data_path);
-            fflush(stdout);
         } else if (!strcmp(argv[i],"--test-data")){
             test_feature_path = (char *) malloc(strlen(argv[i+1]) * sizeof(char));
             strcpy(test_feature_path, argv[i+1]);
-            printf("test_data_path = %s\n", test_feature_path);
-            fflush(stdout);
         } else if (!strcmp(argv[i],"-f")){
             num_arg = atoi(argv[i+1]) + 1;
             mw_replicated_init(&featureSetSize, num_arg);
-            printf("featureSetSize = %ld\n", featureSetSize);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--train-samples")){
             num_arg = atoi(argv[i+1]);
             mw_replicated_init(&train_sample_count, num_arg);
-            printf("train_sample_count = %ld\n", train_sample_count);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--class-values")){
             num_arg = atoi(argv[i+1]);
             class1 = num_arg;
             num_arg = atoi(argv[i+2]);
             class2 = num_arg;
-            printf("classes: {%ld,%ld}\n", class1, class2);
-            fflush(stdout);
             non_standard_classes = 1;
             i+=2;
         } else if (!strcmp(argv[i],"-e")){
             num_arg = atoi(argv[i+1]);
             mw_replicated_init(&total_epochs, num_arg);
-            printf("total_epochs = %ld\n", total_epochs);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--train-points")){
             num_arg = atoi(argv[i+1])+train_sample_count;
             mw_replicated_init(&total_train_points, num_arg);
-            printf("total_train_points = %ld\n", total_train_points);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--test-samples")){
 	        num_arg = atoi(argv[i+1]);
 	        mw_replicated_init(&test_sample_count, num_arg);
-	        printf("test_sample_count = %ld\n", test_sample_count);
-	        fflush(stdout);
 	        i++;
         } else if (!strcmp(argv[i],"--test-points")){
 	        num_arg = atoi(argv[i+1])+test_sample_count;
 	        mw_replicated_init(&total_test_points, num_arg);
-	        printf("total_test_points = %ld\n", total_test_points);
-	        fflush(stdout);
 	        i++;
         } else if (!strcmp(argv[i],"--initial-step-size")){
             sscanf(argv[i+1],"%lf",&scaled_float);
             double d_temp = scaled_float * 16777216;
             long eta_init = (long) d_temp;
             mw_replicated_init(&eta, eta_init);
-            printf("initial step size: %lf\n", scaled_float);
-            fflush(stdout);
             i++;
         } else if (!strcmp(argv[i],"--initial-step-decay")){
             sscanf(argv[i+1],"%lf", &scaled_float);
             double d_temp = scaled_float * 16777216;
             long gamma_init = (long) d_temp;
             mw_replicated_init(&gamma, gamma_init);
-            printf("initial_step_decay = %lf\n", scaled_float);
-            fflush(stdout);
             i++;
         }
     }
@@ -114,9 +90,6 @@ void parse_args(int argc, char * argv[]) {
  * was done for simplicity.
  */
 void populateTrainingData() {
-    printf("inside populate_data()\n");
-    fflush(stdout);
-
     long i,
             sample = -1,
             feature,
@@ -127,7 +100,6 @@ void populateTrainingData() {
     long sample_count = -1;
     training_sample_indicies[0] = 0;
     MIGRATE(&model_vector[0]);
-
     train_data = NULL;
     train_data = fopen(train_data_path, "rb");
     if (train_data == NULL) {
@@ -139,12 +111,7 @@ void populateTrainingData() {
     long points;
     long *binBuffer;
     long bytesRead;
-
-
     points = non_zeros * 4;
-    printf("points = %ld\n", points);
-    fflush(stdout);
-
     binBuffer = (long *) malloc(points * sizeof(long));
     bytesRead = fread(binBuffer, sizeof(long), points, train_data);
 
@@ -209,7 +176,6 @@ void populateTrainingData() {
 
 void init() {
     long *lPtr;
-
     /** Allocate stripped arrays for model vectors and training sample data */
     lPtr = (long *) mw_malloc1dlong(featureSetSize);
     mw_replicated_init((long *) &model_vector, (long) lPtr);
@@ -233,9 +199,6 @@ void init() {
     mw_replicated_init((long *) &test_values, (long) lPtr);
     lPtr = (long *) mw_malloc1dlong(test_sample_count);
     mw_replicated_init((long *) &test_classifications, (long) lPtr);
-
-    printf("--- Memmory Allocation Complete ---\n");
-	fflush(stdout);
 
 	/** Zero-out allocated memory regions prior to running. This will be replaced with memset or its equivalent in
 	 * the near future
