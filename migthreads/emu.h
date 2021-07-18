c0625bf82d2b33528ff9918b1d7c781f2a739f4f
Brian Page <bripage@users.noreply.github.com>
2021-07-18 15:50:58 -0400

    .

--- migthreads/emu.h
+++ migthreads/emu.h
@@ -1,24 +1,13 @@
 //
 // Created by bpage1 on 4/3/20.
 //
-
 #ifndef EMUSGD_EMU_H
 #define EMUSGD_EMU_H
-//
-// Main header file for EMU
-//
 
 #include "memoryweb.h"
 #include "malloc.h"
-#include "distributed.h"
 #include "cilk.h"
 #include "timing.h"
 #include "stdio.h"
-
-//#define DEBUG 0
-
-#ifndef CLOCK_RATE
-#define CLOCK_RATE 175000000
-#endif
 
 #endif //EMUSGD_EMU_H
